8bd024f35e322a4b49447ec5c8f4045b3eba9547
David Renshaw <dwrenshaw@gmail.com>
2013-07-07 10:21:27 -0400

    structsize

--- addressbook_capnp.rs
+++ addressbook_capnp.rs
@@ -1,6 +1,6 @@
 pub mod Person {
     use layout::*;
-    use addressbook_capnp::*;
+//    use addressbook_capnp::*;
 
     pub struct Reader<'self> {
         _reader : StructReader<'self>
@@ -17,11 +17,14 @@ pub mod Person {
         }
     }
 
+    pub struct Builder {
+        _builder : StructBuilder
+    }
 
+    list_submodule!(addressbook_capnp, Person)
 }
 
 pub mod AddressBook {
-
     use layout::*;
     use addressbook_capnp::*;
 
@@ -38,7 +41,29 @@ pub mod AddressBook {
         pub fn totalSizeInWords(&self) -> uint {
             self._reader.totalSize() as uint
         }
+
+        pub fn getPeople(&self) -> Person::List::Reader<'self> {
+            Person::List::Reader::new(self._reader.getListField(0, INLINE_COMPOSITE, 0))
+        }
     }
 
+    pub struct Builder {
+        _builder : StructBuilder
+    }
 
+    impl Builder {
+        pub fn new(builder : StructBuilder) -> Builder {
+            Builder { _builder : builder }
+        }
+
+/*
+        pub fn initRoot() -> Builder {
+        }
+*/
+    }
+
+    pub static STRUCT_SIZE : StructSize = StructSize {data : 0, pointers : 1,
+                                                      preferredListEncoding : POINTER};
+
+    list_submodule!(addressbook_capnp, AddressBook)
 }
