c38d0a83da9d88caaba6abc0f877486270587bc7
David Renshaw <dwrenshaw@gmail.com>
2013-07-07 20:32:38 -0400

    builder for a union

--- addressbook_capnp.rs
+++ addressbook_capnp.rs
@@ -2,6 +2,11 @@ pub mod Person {
     use layout::*;
 //    use addressbook_capnp::*;
 
+    pub static STRUCT_SIZE : StructSize = StructSize {data : 1, pointers : 4,
+                                                      preferredListEncoding : INLINE_COMPOSITE};
+
+    list_submodule!(addressbook_capnp, Person)
+
     pub struct Reader<'self> {
         _reader : StructReader<'self>
     }
@@ -33,12 +38,43 @@ pub mod Person {
         pub fn setName(&self, value : &str) {
             self._builder.setTextField(0, value);
         }
+
+        pub fn getEmployment(&self) -> Employment::Builder {
+            Employment::Builder::new(self._builder)
+        }
     }
 
-    pub static STRUCT_SIZE : StructSize = StructSize {data : 1, pointers : 4,
-                                                      preferredListEncoding : INLINE_COMPOSITE};
+    pub mod Employment {
+        use layout::*;
 
-    list_submodule!(addressbook_capnp, Person)
+        pub struct Builder {
+            _builder : StructBuilder
+        }
+
+        impl Builder {
+            pub fn new(builder : StructBuilder) -> Builder {
+                Builder { _builder : builder }
+            }
+
+            pub fn setUnemployed(&self) {
+                self._builder.setDataField::<u16>(2, 0);
+            }
+
+            pub fn setEmployer(&self, value : &str) {
+                self._builder.setDataField::<u16>(2, 1);
+                self._builder.setTextField(3, value);
+            }
+
+            pub fn setSchool(&self, value : &str) {
+                self._builder.setDataField::<u16>(2, 2);
+                self._builder.setTextField(3, value);
+            }
+
+            pub fn setSelfEmployed(&self) {
+                self._builder.setDataField::<u16>(2, 3);
+            }
+        }
+    }
 
     pub mod PhoneNumber {
         use layout::*;
