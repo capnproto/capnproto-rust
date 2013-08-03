0297b16815dfa908479bf26978971d1f717f49ca
David Renshaw <dwrenshaw@gmail.com>
2013-08-03 14:29:05 -0400

    getting started on benchmarking

---
+++ benchmark/benchmark.rs
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2013, David Renshaw (dwrenshaw@gmail.com)
+ *
+ * See the LICENSE file in the capnproto-rust root directory.
+ */
+
+#[link(name = "capnproto-rust-benchmark", vers = "alpha", author = "dwrensha")];
+
+#[crate_type = "bin"];
+
+
+use std::libc::*;
+
+pub fn main () {
+
+    let args = std::os::args();
+
+    if (args.len() != 5) {
+        printfln!("USAGE: %s MODE REUSE COMPRESSION ITERATION_COUNT", args[0]);
+        return;
+    }
+
+    let iters = match std::u64::from_str(args[4]) {
+        Some (n) => n,
+        None => {
+            printfln!("Could not parse a u64 from: %s", args[4]);
+            return;
+        }
+    };
+
+    unsafe {
+        let child = funcs::posix88::unistd::fork();
+        if (child == 0 ) {
+            printfln!("%s", "Hello world. I am the child and client.");
+        } else {
+            printfln!("%s", "Hello world. I am the parent and server.");
+        }
+    }
+}
