b8f752db3ef18787cf50973268fa128477625047
David Renshaw <dwrenshaw@gmail.com>
2013-09-19 18:56:08 -0400

    a start on makeExpression

--- benchmark/eval.rs
+++ benchmark/eval.rs
@@ -12,3 +12,20 @@ use eval_capnp::*;
 pub type RequestBuilder = Expression::Builder;
 pub type ResponseBuilder = EvaluationResult::Builder;
 pub type Expectation = i32;
+
+fn makeExpression(rng : &mut FastRand, exp : Expression::Builder, depth : uint) -> i32 {
+    exp.setOp(unsafe {
+            std::cast::transmute(rng.gen_uint_range(0, Operation::modulus as uint + 1))});
+
+    let mut left : u32 = 0;
+    let mut right : u32 = 0;
+
+    if (rng.nextLessThan(8) < (depth as u32)) {
+        left = rng.nextLessThan(128) + 1;
+        exp.getLeft().setValue(left as i32);
+    } else {
+        // how are we going to do this?
+    }
+
+    0
+}
