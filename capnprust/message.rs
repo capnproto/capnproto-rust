07bfe4110b9a961393d050b541f758a1e16de4ad
David Renshaw <dwrenshaw@gmail.com>
2013-11-03 10:08:39 -0500

    remove obsolete comment

--- capnprust/message.rs
+++ capnprust/message.rs
@@ -63,12 +63,7 @@ pub struct MessageBuilder {
     nextSize : uint,
     allocationStrategy : AllocationStrategy,
     segmentBuilders : ~[~SegmentBuilder],
-
     segments : ~[~[u8]]
-    // It would probably be nicer if this were a vector of @mut[u8]s
-    // and the SegmentBuilders also directly held their respective
-    // @mut[u8]s. Only problem is, I don't know how to implement
-    // `asReader` in that case.
 }
 
 impl MessageBuilder {
