7b803dc90df3634e99fd0cfcefa307931db930a9
David Renshaw <dwrenshaw@gmail.com>
2013-11-05 19:46:59 -0500

    implement BufferedOutputStream

--- capnprust/io.rs
+++ capnprust/io.rs
@@ -4,6 +4,7 @@
  * See the LICENSE file in the capnproto-rust root directory.
  */
 
+use std;
 use std::rt::io::Writer;
 
 pub struct BufferedOutputStream<W> {
@@ -13,18 +14,56 @@ pub struct BufferedOutputStream<W> {
 }
 
 impl<W: Writer> BufferedOutputStream<W> {
-    pub fn getWriteBuffer<'a>(&'a mut self) -> &'a mut [u8] {
-        fail!("unimplemented")
+
+    #[inline]
+    pub fn getWriteBuffer(&mut self) -> (*mut u8, uint) {
+        unsafe {
+            (self.buf.unsafe_mut_ref(self.pos), self.pos)
+        }
+    }
+
+    pub fn advance(&mut self, n : uint) {
+        self.pos += n;
+        assert!(self.pos < self.buf.len());
     }
 }
 
+
 impl<W: Writer> Writer for BufferedOutputStream<W> {
-    fn write(&mut self, _buf: &[u8]) {
-        // like BufferedWriter, but first check if buf is a prefix of our inner buf.
-        fail!("unimplemented");
+    fn write(&mut self, buf: &[u8]) {
+        let available = self.buf.len() - self.pos;
+        let mut size = buf.len();
+        if size <= available {
+            let dst = self.buf.mut_slice_from(self.pos);
+            std::vec::bytes::copy_memory(dst, buf, buf.len());
+            self.pos += size;
+        } else if size <= self.buf.len() {
+            //# Too much for this buffer, but not a full buffer's
+            //# worth, so we'll go ahead and copy.
+            {
+                let dst = self.buf.mut_slice_from(self.pos);
+                std::vec::bytes::copy_memory(dst, buf, available);
+            }
+            self.inner.write(self.buf);
+
+            size -= available;
+            let src = buf.slice(available, buf.len());
+            let dst = self.buf.mut_slice_from(0);
+            std::vec::bytes::copy_memory(dst, src, size);
+            self.pos = size;
+        } else {
+            //# Writing so much data that we might as well write
+            //# directly to avoid a copy.
+            self.inner.write(self.buf.slice(0, self.pos));
+            self.pos = 0;
+            self.inner.write(buf);
+        }
     }
 
     fn flush(&mut self) {
-        fail!("unimplemented")
+        if (self.pos > 0) {
+            self.inner.write(self.buf.slice(0, self.pos));
+            self.pos = 0;
+        }
     }
 }
