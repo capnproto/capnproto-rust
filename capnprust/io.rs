2ff6b99da028365bd44e4506e58b8aa896e528ad
David Renshaw <dwrenshaw@gmail.com>
2013-11-10 18:07:35 -0500

    reorg

--- capnprust/io.rs
+++ capnprust/io.rs
@@ -39,11 +39,15 @@ impl<'self, W: Writer> BufferedOutputStream<'self, W> {
     #[inline]
     pub fn write_ptr(&mut self, ptr: *mut u8, size: uint) {
         unsafe {
-            do std::vec::raw::mut_buf_as_slice::<u8,()>(ptr, size) |buf| {
-                self.write(buf);
+            let easyCase = ptr == self.buf.unsafe_mut_ref(self.pos);
+            if easyCase {
+                self.pos += size;
+            } else {
+                do std::vec::raw::mut_buf_as_slice::<u8,()>(ptr, size) |buf| {
+                    self.write(buf);
+                }
             }
         }
-
     }
 }
 
@@ -51,37 +55,32 @@ impl<'self, W: Writer> BufferedOutputStream<'self, W> {
 impl<'self, W: Writer> Writer for BufferedOutputStream<'self, W> {
     #[inline]
     fn write(&mut self, buf: &[u8]) {
-        let easyCase = unsafe {buf.unsafe_ref(0) == self.buf.unsafe_ref(self.pos)};
-        if easyCase {
-            self.pos += buf.len();
-        } else {
-            let available = self.buf.len() - self.pos;
-            let mut size = buf.len();
-            if size <= available {
+        let available = self.buf.len() - self.pos;
+        let mut size = buf.len();
+        if size <= available {
+            let dst = self.buf.mut_slice_from(self.pos);
+            std::vec::bytes::copy_memory(dst, buf, buf.len());
+            self.pos += size;
+        } else if size <= self.buf.len() {
+            //# Too much for this buffer, but not a full buffer's
+            //# worth, so we'll go ahead and copy.
+            {
                 let dst = self.buf.mut_slice_from(self.pos);
-                std::vec::bytes::copy_memory(dst, buf, buf.len());
-                self.pos += size;
-            } else if size <= self.buf.len() {
-                //# Too much for this buffer, but not a full buffer's
-                //# worth, so we'll go ahead and copy.
-                {
-                    let dst = self.buf.mut_slice_from(self.pos);
-                    std::vec::bytes::copy_memory(dst, buf, available);
-                }
-                self.inner.write(self.buf);
-
-                size -= available;
-                let src = buf.slice(available, buf.len());
-                let dst = self.buf.mut_slice_from(0);
-                std::vec::bytes::copy_memory(dst, src, size);
-                self.pos = size;
-            } else {
-                //# Writing so much data that we might as well write
-                //# directly to avoid a copy.
-                self.inner.write(self.buf.slice(0, self.pos));
-                self.pos = 0;
-                self.inner.write(buf);
+                std::vec::bytes::copy_memory(dst, buf, available);
             }
+            self.inner.write(self.buf);
+
+            size -= available;
+            let src = buf.slice(available, buf.len());
+            let dst = self.buf.mut_slice_from(0);
+            std::vec::bytes::copy_memory(dst, src, size);
+            self.pos = size;
+        } else {
+            //# Writing so much data that we might as well write
+            //# directly to avoid a copy.
+            self.inner.write(self.buf.slice(0, self.pos));
+            self.pos = 0;
+            self.inner.write(buf);
         }
     }
 
