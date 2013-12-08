e9fdb546061af1ca0fe065348b809b6525ccb6a6
David Renshaw <dwrenshaw@gmail.com>
2013-12-07 19:11:31 -0500

    a start on ListList

--- capnp/list.rs
+++ capnp/list.rs
@@ -4,8 +4,22 @@
  * See the LICENSE file in the capnproto-rust root directory.
  */
 
+use layout::{PointerBuilder, PointerReader};
+use common::Word;
+
+pub trait FromPointerReader<'a> {
+    fn get_from_pointer(reader : &PointerReader<'a>, default_value : *Word) -> Self;
+}
+
+pub trait FromPointerBuilder {
+    fn init_pointer(PointerBuilder, uint) -> Self;
+    fn get_from_pointer(builder : PointerBuilder, default_value : *Word) -> Self;
+}
+
 pub mod PrimitiveList {
-    use layout::{ListReader, ListBuilder, PrimitiveElement};
+    use super::{FromPointerReader};
+    use layout::{ListReader, ListBuilder, PointerReader, PrimitiveElement, POINTER};
+    use common::Word;
 
     pub struct Reader<'a, T> {
         reader : ListReader<'a>
@@ -17,6 +31,12 @@ pub mod PrimitiveList {
         }
 
         pub fn size(&self) -> uint { self.reader.size() }
+    }
+
+    impl <'a, T : PrimitiveElement> FromPointerReader<'a> for Reader<'a, T> {
+        fn get_from_pointer(reader : &PointerReader<'a>, default_value : *Word) -> Reader<'a, T> {
+            Reader { reader : reader.get_list(POINTER, default_value) }
+        }
     }
 
     impl <'a, T : PrimitiveElement> Index<uint, T> for Reader<'a, T> {
@@ -148,5 +168,36 @@ pub mod StructList {
         }
     }
 
+
+}
+
+pub mod ListList {
+    use super::{FromPointerReader};
+    use common::Word;
+    use layout::*;
+
+    pub struct Reader<'a, T> {
+        reader : ListReader<'a>
+    }
+
+    impl <'a, T : FromPointerReader<'a>> FromPointerReader<'a> for Reader<'a, T> {
+        fn get_from_pointer(reader : &PointerReader<'a>, default_value : *Word) -> Reader<'a, T> {
+            Reader { reader : reader.get_list(POINTER, default_value) }
+        }
+    }
+
+    impl <'a, T : FromPointerReader<'a>> Index<uint, T> for Reader<'a, T> {
+        fn index(&self, index : &uint) -> T {
+//            assert!(*indext <  self.size());
+//            self.reader.get_pointer_element
+            fail!()
+        }
+    }
+
+
+
+    pub struct Builder<T> {
+        builder : ListBuilder
+    }
 
 }
