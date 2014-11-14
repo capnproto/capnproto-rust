795e064ca7a55732c10efd535eece3fe9af007ca
David Renshaw <dwrenshaw@gmail.com>
2014-11-14 16:14:57 -0500

    use the new separate capnpc-rust Cargo package

--- Makefile
+++ Makefile
@@ -1,23 +1,5 @@
 RUSTC = rustc -O
 
-CAPNP_INCLUDE_DIR=$(shell dirname $(shell which capnp))/../include
-
-DEPS_DIR=$(OUT_DIR)/../../deps
-CAPNP_DEP=$(shell ls $(DEPS_DIR)/libcapnp*.rlib)
-
-.PHONY : generated
-
-generated : $(OUT_DIR)/rpc_capnp.rs
-
-SCHEMA_SOURCES= $(CAPNP_INCLUDE_DIR)/capnp/rpc.capnp $(CAPNP_INCLUDE_DIR)/capnp/rpc-twoparty.capnp
-
-$(OUT_DIR)/rpc_capnp.rs : $(SCHEMA_SOURCES) $(CAPNP_DEP)
-	capnp compile -orust:$(OUT_DIR) --src-prefix=$(CAPNP_INCLUDE_DIR)/capnp \
-      $(CAPNP_INCLUDE_DIR)/capnp/rpc.capnp $(CAPNP_INCLUDE_DIR)/capnp/rpc-twoparty.capnp
-	cp capnp_rpc_include_generated.rs $(OUT_DIR)
-	rustc -L$(DEPS_DIR) $(OUT_DIR)/capnp_rpc_include_generated.rs --out-dir $(OUT_DIR)
-
-
 examples/calculator/calculator :  examples/calculator/main.rs \
 	                             examples/calculator/client.rs examples/calculator/server.rs \
                                  examples/calculator/calculator.capnp
--- build.rs
+++ build.rs
@@ -1,28 +1,19 @@
-use std::os;
-use std::io::Command;
+extern crate capnpc;
 
 fn main() {
 
     // CAPNP_INCLUDE_DIR=$(shell dirname $(shell which capnp))/../include
 
-    let include_dir = {
-        let output = Command::new("which").arg("capnp")
+    let prefix = {
+        let output = ::std::io::Command::new("which").arg("capnp")
             .output().unwrap().output;
         let path = Path::new(output.as_slice());
         let mut path1 = Path::new(path.dirname());
-        path1.push("../include");
-        format!("{}", path1.display())
+        path1.push("../include/capnp");
+        path1
     };
 
-    let out_dir = os::getenv("OUT_DIR").unwrap();
-
-    let _output = Command::new("capnp")
-        .arg("compile")
-        .arg(format!("-orust:{}", out_dir))
-        .arg(format!("--src-prefix={}/capnp", include_dir))
-        .arg(format!("{}/capnp/rpc.capnp", include_dir))
-        .arg(format!("{}/capnp/rpc-twoparty.capnp", include_dir))
-        .output()
-        .unwrap();
-
+    ::capnpc::compile(prefix.clone(),
+                      vec!(prefix.join(Path::new("rpc.capnp")),
+                           prefix.join(Path::new("rpc-twoparty.capnp"))).as_slice());
 }
