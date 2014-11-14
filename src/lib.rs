ecdd726c94e51531521b071f0f5be9534b95700a
David Renshaw <dwrenshaw@gmail.com>
2014-11-14 15:05:10 -0500

    fill in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -14,3 +14,31 @@ extern crate capnp;
 pub mod schema_capnp;
 pub mod codegen;
 
+pub fn compile(prefix : Path, files : &[Path]) {
+
+    let out_dir = ::std::os::getenv("OUT_DIR").unwrap();
+
+    let mut command = ::std::io::Command::new("capnp");
+    command
+        .arg("compile")
+        .arg(format!("-o/bin/cat:{}", out_dir))
+        .arg(format!("--src-prefix={}", prefix.display()));
+
+    for file in files.iter() {
+        command.arg(format!("{}", prefix.join(file).display()));
+    }
+
+    command.stdout(::std::io::process::CreatePipe(false, true));
+
+    match command.spawn() {
+        Ok(ref mut p) =>  {
+            let mut child_stdout = p.stdout.take().unwrap();
+            ::codegen::main(&mut child_stdout).unwrap();
+        }
+        Err(e) => {
+            println!("could not start process: {}", e);
+        }
+    }
+
+}
+
