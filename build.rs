fd287606b468d27f3114c81614558004581e0270
David Renshaw <dwrenshaw@gmail.com>
2015-03-14 11:09:40 -0400

    io and path are now stable

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-#![feature(core, path)]
+#![feature(core)]
 
 extern crate capnpc;
 
--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@
 #![crate_name="capnp-rpc"]
 #![crate_type="lib"]
 
-#![feature(box_syntax, core, io, net)]
+#![feature(box_syntax, core, net)]
 
 extern crate core;
 extern crate capnp;
