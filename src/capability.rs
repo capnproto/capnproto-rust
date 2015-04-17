f99ffbb5f41e4ff44722944deaacf5ca532dc07a
David Renshaw <dwrenshaw@gmail.com>
2015-04-17 08:46:59 -0400

    Delete the PhantomFn compatibility hack.

--- src/capability.rs
+++ src/capability.rs
@@ -90,7 +90,6 @@ impl ServerHook for LocalClient {
 }
 
 pub trait InitRequest<'a, 'b, T> {
-    fn phantom(&'a()) { panic!("This function only exists for variance.") }
     fn init(&'b mut self) -> T;
 }
 
