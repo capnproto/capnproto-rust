8d0f10cd1954fe08b9b9a37e1679e4051df9ce67
David Renshaw <dwrenshaw@gmail.com>
2015-05-23 13:46:30 -0400

    Only generate documentation for the capnp packages.

--- publish-docs.sh
+++ publish-docs.sh
@@ -5,7 +5,9 @@ set -x
 
 PUBLISH_DIR=~/docs.capnproto-rust.org
 
-cargo doc
+cargo doc --no-deps -p capnp
+cargo doc --no-deps -p capnpc
+cargo doc --no-deps -p capnp-rpc
 rm -rf $PUBLISH_DIR/*
 cp -r target/doc/* $PUBLISH_DIR/
 
