148e3867f540566b4bc8c463f79f4e45f5ceee46
Adolfo Ochagavía <aochagavia92@gmail.com>
2015-05-30 11:27:57 +0200

    Use AsRef in the compile function

--- example/addressbook/build.rs
+++ example/addressbook/build.rs
@@ -1,6 +1,5 @@
 extern crate capnpc;
 
 fn main() {
-    ::capnpc::compile(&::std::path::Path::new("."),
-                      &[::std::path::Path::new("addressbook.capnp")]).unwrap();
+    ::capnpc::compile(".", &["addressbook.capnp"]).unwrap();
 }
