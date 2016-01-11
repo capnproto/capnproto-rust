fe61c0e73e1c82d23640b5eac0f89199646edbf1
David Renshaw <david@sandstorm.io>
2016-01-11 13:45:42 -0500

    include gj in published docs

--- publish-docs.sh
+++ publish-docs.sh
@@ -8,6 +8,7 @@ PUBLISH_DIR=~/docs.capnproto-rust.org
 cargo doc --no-deps -p capnp
 cargo doc --no-deps -p capnpc
 cargo doc --no-deps -p capnp-rpc
+cargo doc --no-deps -p gj
 rm -rf $PUBLISH_DIR/*
 cp -r target/doc/* $PUBLISH_DIR/
 
