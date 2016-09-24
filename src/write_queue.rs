2637ea9cad984d7577cb0dbafb852a1f27c5f195
David Renshaw <david@sandstorm.io>
2016-09-24 11:04:27 -0400

    fill in the rest of WriteQueue

--- src/write_queue.rs
+++ src/write_queue.rs
@@ -20,7 +20,7 @@
 
 use std::io;
 use std::collections::VecDeque;
-use futures::{self, stream, task, Future, Poll, Complete, Oneshot};
+use futures::{self, task, Async, Future, Poll, Complete, Oneshot};
 
 use capnp::{Error};
 
@@ -29,11 +29,13 @@ use serialize::{self, AsOutputSegments};
 
 enum State<W, M> where W: io::Write, M: AsOutputSegments {
     Writing(serialize::Write<W, M>, Complete<M>),
-    Empty(W),
+    BetweenWrites(W),
+    Empty,
 }
 
+/// A write of messages being written.
 pub struct WriteQueue<W, M> where W: io::Write, M: AsOutputSegments {
-    queue: VecDeque<(M, Complete<Result<M, Error>>)>,
+    queue: VecDeque<(M, Complete<M>)>,
     state: State<W, M>,
     task: Option<task::Task>,
 }
@@ -42,39 +44,81 @@ impl <W, M> WriteQueue<W, M> where W: io::Write, M: AsOutputSegments {
     pub fn new(writer: W) -> WriteQueue<W, M> {
         WriteQueue {
             queue: VecDeque::new(),
-            state: State::Empty(writer),
+            state: State::BetweenWrites(writer),
             task: None,
         }
     }
 
-    pub fn push(&mut self, message: M) -> Oneshot<Result<M,Error>> {
+    /// Enqueues a message to be written.
+    pub fn push(&mut self, message: M) -> Oneshot<M> {
         let (complete, oneshot) = futures::oneshot();
-
-        // If Empty, then transition to Writing....
         self.queue.push_back((message, complete));
+
+        match self.task.take() {
+            Some(t) => t.unpark(),
+            None => (),
+        }
 
         oneshot
     }
+
+    /// Returns the number of messages queued to be written, including an in-progress write.
+    pub fn len(&mut self) -> usize {
+        let in_progress = if let State::Writing(..) = self.state { 1 } else { 0 };
+        self.queue.len() + in_progress
+    }
 }
 
-impl <W, M> stream::Stream for WriteQueue<W, M> where W: io::Write, M: AsOutputSegments {
-    type Item = M;
+enum IntermediateState<W, M> where W: io::Write, M: AsOutputSegments {
+    WriteDone(M, W),
+    StartWrite(M, Complete<M>),
+}
+
+impl <W, M> Future for WriteQueue<W, M> where W: io::Write, M: AsOutputSegments {
+    type Item = (); // Should never actually terminate.
     type Error = Error;
 
-    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        match self.state {
-            State::Writing(ref mut write, ref mut _complete) => {
-                let (_w, _m) = try_ready!(Future::poll(write));
-                // complete.complete(m) ...
-                ()
-            }
-            State::Empty(ref mut _writer) => {
-                // if queue is empty, park task.
-                // otherwise, grab something off the queue and start writing it.
-                unimplemented!()
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        loop {
+            let next = match self.state {
+                State::Writing(ref mut write, ref mut _complete) => {
+                    let (w, m) = try_ready!(Future::poll(write));
+                    IntermediateState::WriteDone(m, w)
+                }
+                State::BetweenWrites(ref mut _writer) => {
+                    match self.queue.pop_front() {
+                        Some((m, complete)) => {
+                            IntermediateState::StartWrite(m, complete)
+                        }
+                        None => {
+                            // if queue is empty, park task.
+                            self.task = Some(task::park());
+                            return Ok(Async::NotReady)
+                        }
+                    }
+                }
+                State::Empty => unreachable!(),
+            };
+
+            match next {
+                IntermediateState::WriteDone(m, w) => {
+                    match ::std::mem::replace(&mut self.state, State::BetweenWrites(w)) {
+                        State::Writing(_, complete) => {
+                            complete.complete(m)
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+                IntermediateState::StartWrite(m, c) => {
+                    let new_state = match ::std::mem::replace(&mut self.state, State::Empty) {
+                        State::BetweenWrites(w) => {
+                            State::Writing(::serialize::write_message(w, m), c)
+                        }
+                        _ => unreachable!(),
+                    };
+                    self.state = new_state;
+                }
             }
         }
-
-        unimplemented!()
     }
 }
