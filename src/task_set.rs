f8d268a03e6fa08c7dad1898bc63e6d2b300b0a2
David Renshaw <david@sandstorm.io>
2016-12-12 21:56:09 -0500

    actually hook up the reaper

--- src/task_set.rs
+++ src/task_set.rs
@@ -21,17 +21,21 @@
 use futures::{Future, Stream};
 use futures::sync::oneshot;
 
+use std::cell::{RefCell};
+use std::rc::{Rc};
+
 pub struct TaskSet<T, E> {
-    sender: ::futures::sync::mpsc::UnboundedSender<Box<Future<Item=T,Error=E>>>,
+    sender: ::futures::sync::mpsc::UnboundedSender<Box<Future<Item=(),Error=()>>>,
     _canceler: ::futures::sync::oneshot::Sender<()>, // when dropped, the tasks get canceled
+    reaper: Rc<RefCell<Box<TaskReaper<T, E>>>>,
 }
 
-impl<T, E> TaskSet<T, E> {
-    pub fn new(_reaper: Box<TaskReaper<T, E>>, handle: &::tokio_core::reactor::Handle)
+impl<T, E> TaskSet<T, E> where T: 'static, E: 'static {
+    pub fn new(reaper: Box<TaskReaper<T, E>>, handle: &::tokio_core::reactor::Handle)
                -> TaskSet<T, E>
         where E: 'static, T: 'static, E: ::std::fmt::Debug,
     {
-        let (tx, rx) = ::futures::sync::mpsc::unbounded::<Box<Future<Item=T,Error=E>>>();
+        let (tx, rx) = ::futures::sync::mpsc::unbounded::<Box<Future<Item=(),Error=()>>>();
         let stream = rx.map_err(|()| unreachable!())
             .buffer_unordered(1000); // XXX hack that should basically work in small cases.
 
@@ -39,20 +43,29 @@ impl<T, E> TaskSet<T, E> {
         let dropped = dropped.map_err(|_| ());
 
         let f = dropped.join(
-            stream.for_each(|_t| Ok(()) ).map_err(|e| { println!("error {:?}", e); ()})).map(|_| {println!("task set done"); ()});
+            stream.for_each(|()| Ok(()) ).map_err(|e| { println!("error {:?}", e); ()})).map(|_| {println!("task set done"); ()});
 
         handle.spawn(f);
 
         TaskSet {
             sender: tx,
             _canceler: fulfiller,
+            reaper: Rc::new(RefCell::new(reaper)),
         }
     }
 
     pub fn add<F>(&mut self, promise: F)
         where F: Future<Item=T, Error=E> + 'static
     {
-        self.sender.send(Box::new(promise)).unwrap();
+        let reaper = self.reaper.clone();
+        let task = promise.then(move |r| {
+            match r {
+                Ok(v) => reaper.borrow_mut().task_succeeded(v),
+                Err(e) => reaper.borrow_mut().task_failed(e),
+            }
+            Ok(())
+        });
+        self.sender.send(Box::new(task)).unwrap();
     }
 }
 
