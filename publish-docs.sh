1cdcb0e3880c993fd249a6402d86a8a88a706eb9
David Renshaw <david@sandstorm.io>
2017-01-04 14:14:05 -0500

    update docs publisher script

--- publish-docs.sh
+++ publish-docs.sh
@@ -8,8 +8,7 @@ PUBLISH_DIR=~/docs.capnproto-rust.org
 cargo doc --no-deps -p capnp
 cargo doc --no-deps -p capnpc
 cargo doc --no-deps -p capnp-rpc
-cargo doc --no-deps -p gj
-cargo doc --no-deps -p gjio
+cargo doc --no-deps -p capnp-futures
 rm -rf $PUBLISH_DIR/*
 cp -r target/doc/* $PUBLISH_DIR/
 
