53d7ab2ccab3c1f32b9c51b274358aa5ee6a8dd5
David Renshaw <david@sandstorm.io>
2017-02-15 11:43:55 -0500

    style

--- src/capability.rs
+++ src/capability.rs
@@ -23,13 +23,14 @@
 //!
 //! Roughly corresponds to capability.h in the C++ implementation.
 
-use any_pointer;
+use {any_pointer, Error, MessageSize};
+use traits::{Pipelined, Owned};
 use private::capability::{ClientHook, ParamsHook, RequestHook, ResponseHook, ResultsHook};
 
 #[cfg(feature = "rpc")]
 use futures::Future;
 
-use std::{marker};
+use std::marker::PhantomData;
 
 /// A computation that might eventually resolve to a value of type `T` or to an error
 ///  of type `E`. Dropping the promise cancels the computation.
@@ -89,60 +90,60 @@ impl <T, E> Future for Promise<T, E>
 
 /// A promise for a result from a method call.
 #[must_use]
-pub struct RemotePromise<Results> where Results: ::traits::Pipelined + for<'a> ::traits::Owned<'a> + 'static {
+pub struct RemotePromise<Results> where Results: Pipelined + for<'a> Owned<'a> + 'static {
     pub promise: Promise<Response<Results>, ::Error>,
     pub pipeline: Results::Pipeline,
 }
 
 /// A response from a method call, as seen by the client.
 pub struct Response<Results> {
-    pub marker: marker::PhantomData<Results>,
+    pub marker: PhantomData<Results>,
     pub hook: Box<ResponseHook>,
 }
 
 impl <Results> Response<Results>
-    where Results: ::traits::Pipelined + for<'a> ::traits::Owned<'a>
+    where Results: Pipelined + for<'a> Owned<'a>
 {
     pub fn new(hook: Box<ResponseHook>) -> Response<Results> {
-        Response { marker: marker::PhantomData, hook: hook }
+        Response { marker: PhantomData, hook: hook }
     }
-    pub fn get<'a>(&'a self) -> ::Result<<Results as ::traits::Owned<'a>>::Reader> {
+    pub fn get<'a>(&'a self) -> ::Result<<Results as Owned<'a>>::Reader> {
         try!(self.hook.get()).get_as()
     }
 }
 
 /// A method call that has not been sent yet.
 pub struct Request<Params, Results> {
-    pub marker: marker::PhantomData<(Params, Results)>,
+    pub marker: PhantomData<(Params, Results)>,
     pub hook: Box<RequestHook>
 }
 
-impl <Params, Results> Request <Params, Results>
-    where Params: for<'a> ::traits::Owned<'a>
+impl <Params, Results> Request<Params, Results>
+    where Params: for<'a> Owned<'a>
 {
     pub fn new(hook: Box<RequestHook>) -> Request <Params, Results> {
-        Request { hook: hook, marker: marker::PhantomData }
+        Request { hook: hook, marker: PhantomData }
     }
 
-    pub fn get<'a>(&'a mut self) -> <Params as ::traits::Owned<'a>>::Builder {
+    pub fn get<'a>(&'a mut self) -> <Params as Owned<'a>>::Builder {
         self.hook.get().get_as().unwrap()
     }
 
-    pub fn set(&mut self, from: <Params as ::traits::Owned>::Reader) -> ::Result<()> {
+    pub fn set(&mut self, from: <Params as Owned>::Reader) -> ::Result<()> {
         self.hook.get().set_as(from)
     }
 }
 
 #[cfg(feature = "rpc")]
 impl <Params, Results> Request <Params, Results>
-where Results: ::traits::Pipelined + for<'a> ::traits::Owned<'a> + 'static,
-      <Results as ::traits::Pipelined>::Pipeline: FromTypelessPipeline
+where Results: Pipelined + for<'a> Owned<'a> + 'static,
+      <Results as Pipelined>::Pipeline: FromTypelessPipeline
 {
     pub fn send(self) -> RemotePromise<Results> {
         let RemotePromise {promise, pipeline, ..} = self.hook.send();
         let typed_promise = Promise::from_future(promise.map(|response| {
             Response {hook: response.hook,
-                      marker: marker::PhantomData}
+                      marker: PhantomData}
         }));
         RemotePromise { promise: typed_promise,
                         pipeline: FromTypelessPipeline::new(pipeline)
@@ -152,16 +153,16 @@ where Results: ::traits::Pipelined + for<'a> ::traits::Owned<'a> + 'static,
 
 /// The values of the parameters passed to a method call, as seen by the server.
 pub struct Params<T> {
-    pub marker: marker::PhantomData<T>,
+    pub marker: PhantomData<T>,
     pub hook: Box<ParamsHook>,
 }
 
 impl <T> Params <T> {
     pub fn new(hook: Box<ParamsHook>) -> Params<T> {
-        Params { marker: marker::PhantomData, hook: hook }
+        Params { marker: PhantomData, hook: hook }
     }
-    pub fn get<'a>(&'a self) -> ::Result<<T as ::traits::Owned<'a>>::Reader>
-        where T: ::traits::Owned<'a>
+    pub fn get<'a>(&'a self) -> ::Result<<T as Owned<'a>>::Reader>
+        where T: Owned<'a>
     {
         Ok(try!(try!(self.hook.get()).get_as()))
     }
@@ -169,22 +170,22 @@ impl <T> Params <T> {
 
 /// The return values of a method, written in-place by the method body.
 pub struct Results<T> {
-    pub marker: marker::PhantomData<T>,
+    pub marker: PhantomData<T>,
     pub hook: Box<ResultsHook>,
 }
 
 impl <T> Results<T>
-    where T: for<'a> ::traits::Owned<'a>
+    where T: for<'a> Owned<'a>
 {
     pub fn new(hook: Box<ResultsHook>) -> Results<T> {
-        Results { marker: marker::PhantomData, hook: hook }
+        Results { marker: PhantomData, hook: hook }
     }
 
-    pub fn get<'a>(&'a mut self) -> <T as ::traits::Owned<'a>>::Builder {
+    pub fn get<'a>(&'a mut self) -> <T as Owned<'a>>::Builder {
         self.hook.get().unwrap().get_as().unwrap()
     }
 
-    pub fn set(&mut self, other: <T as ::traits::Owned>::Reader) -> ::Result<()>
+    pub fn set(&mut self, other: <T as Owned>::Reader) -> ::Result<()>
     {
         self.hook.get().unwrap().set_as(other)
     }
@@ -212,10 +213,10 @@ impl Client {
     pub fn new_call<Params, Results>(&self,
                                      interface_id : u64,
                                      method_id : u16,
-                                     size_hint : Option<::MessageSize>)
+                                     size_hint : Option<MessageSize>)
                                      -> Request<Params, Results> {
         let typeless = self.hook.new_call(interface_id, method_id, size_hint);
-        Request { hook: typeless.hook, marker: marker::PhantomData }
+        Request { hook: typeless.hook, marker: PhantomData }
     }
 
     /// If the capability is actually only a promise, the returned promise resolves once the
@@ -224,7 +225,7 @@ impl Client {
     /// where no calls are being made.  There is no reason to wait for this before making calls; if
     /// the capability does not resolve, the call results will propagate the error.
     #[cfg(feature = "rpc")]
-    pub fn when_resolved(&self) -> Promise<(), ::Error> {
+    pub fn when_resolved(&self) -> Promise<(), Error> {
         self.hook.when_resolved()
     }
 }
@@ -234,6 +235,6 @@ pub trait Server {
     fn dispatch_call(&mut self, interface_id: u64, method_id: u16,
                      params: Params<any_pointer::Owned>,
                      results: Results<any_pointer::Owned>)
-                     -> Promise<(), ::Error>;
+                     -> Promise<(), Error>;
 }
 
