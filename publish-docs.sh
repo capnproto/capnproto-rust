47bc0387ffd8acd179ba954191bdbf1a68dfdce8
David Renshaw <david@sandstorm.io>
2017-02-19 13:23:58 -0500

    add PUBLISH_USER in publish-docs.sh

--- publish-docs.sh
+++ publish-docs.sh
@@ -3,12 +3,13 @@
 set -e
 set -x
 
-PUBLISH_DIR=~/docs.capnproto-rust.org
+PUBLISH_DIR=/srv/http/docs.capnproto-rust.org
+PUBLISH_USER=http
 
 cargo doc --no-deps -p capnp
 cargo doc --no-deps -p capnpc
 cargo doc --no-deps -p capnp-rpc
 cargo doc --no-deps -p capnp-futures
-rm -rf $PUBLISH_DIR/*
-cp -r target/doc/* $PUBLISH_DIR/
+sudo -u $PUBLISH_USER rm -rf $PUBLISH_DIR/*
+sudo -u $PUBLISH_USER cp -r target/doc/* $PUBLISH_DIR/
 
