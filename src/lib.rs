193fb7bff69eee4fa2ed6f734be896ef7151fe4c
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2017-02-24 21:54:14 +0100

    Mention capnp prerequisite in documentation

--- src/lib.rs
+++ src/lib.rs
@@ -22,8 +22,10 @@
 //! # Cap'n Proto Schema Compiler Plugin Library
 //!
 //! This library allows you to do [Cap'n Proto code generation]
-//! (https://capnproto.org/otherlang.html#how-to-write-compiler-plugins)
-//! within a Cargo build.
+//! (https://capnproto.org/otherlang.html#how-to-write-compiler-plugins) within
+//! a Cargo build. You still need the `capnp` binary (implemented in C++).
+//! (If you use a package manager, try looking for a package called
+//! `capnproto`.)
 //!
 //! In your Cargo.toml:
 //!
