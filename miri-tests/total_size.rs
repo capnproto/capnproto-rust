aef6b9b228bb7b599761331de48e7383b63a283d
David Renshaw <dwrenshaw@gmail.com>
2017-09-02 16:40:31 -0400

    simplify total_size test

--- miri-tests/total_size.rs
+++ miri-tests/total_size.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate capnp;
 
-fn try_go() -> ::capnp::Result<()> {
+pub fn main() {
     let segment: &[::capnp::Word] = &[
         capnp_word!(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00),
         capnp_word!(0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00),
@@ -10,17 +10,11 @@ fn try_go() -> ::capnp::Result<()> {
     let segments = &[segment];
     let segment_array = capnp::message::SegmentArray::new(segments);
     let message = capnp::message::Reader::new(segment_array, Default::default());
-    let root: capnp::any_pointer::Reader = try!(message.get_root());
+    let root: capnp::any_pointer::Reader = message.get_root().unwrap();
 
     // At one point, this failed with:
     // error: pointer computed at offset 33554448, outside bounds of allocation Runtime(702) which has size 16
     let result = root.target_size();
 
     assert!(result.is_err()); // pointer out-of-bounds error
-
-    Ok(())
-}
-
-pub fn main() {
-    let _ = try_go();
 }
