218e89f491055d7e8bee974d731c6856699565a2
David Renshaw <dwrenshaw@gmail.com>
2018-10-12 22:45:21 -0400

    add a bit more traversal in the test_all_types fuzzer

--- capnp/fuzz/fuzzers/test_all_types.rs
+++ capnp/fuzz/fuzzers/test_all_types.rs
@@ -9,42 +9,60 @@ pub mod test_capnp;
 
 fn traverse(v: test_all_types::Reader) -> ::capnp::Result<()> {
     v.get_int64_field();
-    try!(v.get_text_field());
-    try!(v.get_data_field());
-    try!(v.get_struct_field());
+    v.get_float32_field();
+    v.get_float64_field();
+    v.get_text_field()?;
+    v.get_data_field()?;
+    v.get_struct_field()?;
+    v.get_enum_field()?;
 
-    let structs = try!(v.get_struct_list());
+    let structs = v.get_struct_list()?;
     for s in structs.iter() {
-        try!(s.get_text_field());
+        s.get_text_field()?;
+        s.get_data_field()?;
     }
 
-    try!(v.get_bool_list());
+    let bools = v.get_bool_list()?;
+    for idx in 0 .. bools.len() {
+        bools.get(idx);
+    }
+
+    let enums = v.get_enum_list()?;
+    for idx in 0 .. enums.len() {
+        enums.get(idx)?;
+    }
+
+    let int8s = v.get_int8_list()?;
+    for idx in 0 .. int8s.len() {
+        int8s.get(idx);
+    }
+
     Ok(())
 }
 
 fn try_go(mut data: &[u8]) -> ::capnp::Result<()> {
     let orig_data = data;
-    let message_reader = try!(serialize::read_message(
+    let message_reader = serialize::read_message(
         &mut data,
-        *message::ReaderOptions::new().traversal_limit_in_words(4 * 1024)));
+        *message::ReaderOptions::new().traversal_limit_in_words(4 * 1024))?;
     assert!(orig_data.len() > data.len());
 
     let root: test_all_types::Reader = try!(message_reader.get_root());
-    try!(root.total_size());
-    try!(traverse(root));
+    root.total_size()?;
+    traverse(root)?;
 
     let mut message = message::Builder::new_default();
-    try!(message.set_root(root));
+    message.set_root(root)?;
     {
-        let root_builder = try!(message.get_root::<test_all_types::Builder>());
-        try!(root_builder.total_size());
-        try!(traverse(root_builder.as_reader()));
+        let root_builder = message.get_root::<test_all_types::Builder>()?;
+        root_builder.total_size()?;
+        traverse(root_builder.as_reader())?;
     }
 
     // init_root() will zero the previous value
     let mut new_root = message.init_root::<test_all_types::Builder>();
 
-    try!(new_root.set_struct_field(root));
+    new_root.set_struct_field(root)?;
     Ok(())
 }
 
