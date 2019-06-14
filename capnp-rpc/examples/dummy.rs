5bbb2aacfe79027f77fefe562c103fde466320e9
David Renshaw <dwrenshaw@gmail.com>
2019-06-14 19:55:47 -0400

    set 'autoexamples = false' to avoid need for dummy example

--- capnp-rpc/examples/dummy.rs
+++
@@ -1,6 +0,0 @@
-// Usually 'cargo test' tries to build everything in the examples directory.
-// That doesn't work for our examples because they have their own Cargo.toml and build.rs files.
-// We can override the default behavior of 'cargo test' by supplying a dummy file, like this,
-// and pointing to it in our Cargo.toml's [[example]] section.
-
-fn main() {}
