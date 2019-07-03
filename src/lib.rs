d84de5382738a7299a23c5489d0d6d2a969267eb
real <real@freedomlayer.org>
2019-07-03 16:55:56 +0300

    Removed unused comment.

--- src/lib.rs
+++ src/lib.rs
@@ -57,48 +57,6 @@ pub trait FromCapnpBytes: Sized {
     fn from_capnp_bytes(bytes: &[u8]) -> Result<Self, CapnpConvError>;
 }
 
-// -------------------------------------------------------
-// -------------------------------------------------------
-
-/*
-/// A shim allowing to merge cases where either
-/// Result<T,Into<CapnoConvError>> or a T is returned.
-pub enum CapnpResult<T> {
-    Ok(T),
-    Err(CapnpConvError),
-}
-
-impl<T> CapnpResult<T> {
-    pub fn into_result(self) -> Result<T, CapnpConvError> {
-        match self {
-            CapnpResult::Ok(t) => Ok(t),
-            CapnpResult::Err(e) => Err(e),
-        }
-    }
-}
-
-impl<T> From<T> for CapnpResult<T> {
-    fn from(input: T) -> Self {
-        CapnpResult::Ok(input)
-    }
-}
-
-impl<T, E> From<Result<T, E>> for CapnpResult<T>
-where
-    E: Into<CapnpConvError>,
-{
-    fn from(input: Result<T, E>) -> Self {
-        match input {
-            Ok(t) => CapnpResult::Ok(t),
-            Err(e) => CapnpResult::Err(e.into()),
-        }
-    }
-}
-*/
-
-// -------------------------------------------------------
-// -------------------------------------------------------
-
 impl<T> ToCapnpBytes for T
 where
     T: for<'a> WriteCapnp<'a>,
