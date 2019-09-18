53c427fe3d179b97c64fbc139336263328b41430
real <real@freedomlayer.org>
2019-09-18 17:05:02 +0300

    capnp_conv: Removed old async_await and await_macro features.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![crate_type = "lib"]
-#![feature(async_await, await_macro, arbitrary_self_types)]
+#![feature(arbitrary_self_types)]
 #![feature(nll)]
 #![feature(generators)]
 #![feature(never_type)]
