f75906a361d023a255dec13c2251d3c0cf860e0b
David Renshaw <dwrenshaw@gmail.com>
2020-01-16 18:12:09 -0500

    pull capnp/miri-tests/total_size.rs into capnp/tests, because miri is easy to run on all tests now

--- capnp/miri-tests/README.md
+++
@@ -1,5 +0,0 @@
-Run these tests with:
-
-```
-RUSTFLAGS="-Z always-encode-mir" xargo miri
-```
--- capnp/tests/total_size.rs
+++ capnp/tests/total_size.rs
@@ -1,6 +1,5 @@
-extern crate capnp;
-
-pub fn main() {
+#[test]
+pub fn total_size_out_of_bounds() {
     let segment: &[::capnp::Word] = &[
         ::capnp::word(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00),
         ::capnp::word(0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00),
@@ -11,7 +10,7 @@ pub fn main() {
     let message = capnp::message::Reader::new(segment_array, Default::default());
     let root: capnp::any_pointer::Reader = message.get_root().unwrap();
 
-    // At one point, this failed with:
+    // At one point, this failed in miri with:
     // error: pointer computed at offset 33554448, outside bounds of allocation Runtime(702) which has size 16
     let result = root.target_size();
 
