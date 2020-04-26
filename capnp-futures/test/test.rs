7cb874f4af14278b9712252b0955a244085e57f4
Jannick Johnsen <jannickboese@msn.com>
2020-04-26 08:24:26 +0200

    rm warnings from tests

--- capnp-futures/test/test.rs
+++ capnp-futures/test/test.rs
@@ -173,8 +173,8 @@ mod tests {
 
         let (mut write, mut read) =
             async_std::os::unix::net::UnixStream::pair().expect("socket pair");
-        serialize::read_message(&mut read, message::ReaderOptions::default());
-        serialize::write_message(&mut write, message::Builder::new_default());
+        let _ = serialize::read_message(&mut read, message::ReaderOptions::default());
+        let _ = serialize::write_message(&mut write, message::Builder::new_default());
         drop(write);
         drop(read);
     }
@@ -186,8 +186,8 @@ mod tests {
 
         let (mut write, mut read) =
             async_std::os::unix::net::UnixStream::pair().expect("socket pair");
-        capnp_futures::ReadStream::new(&mut read, message::ReaderOptions::default());
-        capnp_futures::write_queue::<_, message::Builder<message::HeapAllocator>>(&mut write);
+        let _ = capnp_futures::ReadStream::new(&mut read, message::ReaderOptions::default());
+        let _ = capnp_futures::write_queue::<_, message::Builder<message::HeapAllocator>>(&mut write);
         drop(write);
         drop(read);
     }
