0d1fa4890f2545d3310eb9b7aa1c3d3507e08cc1
Jannick Johnsen <jannickboese@msn.com>
2020-04-26 08:24:04 +0200

    redesign static lifetime into generic lifetime requirements for functions/data

--- capnp-futures/src/write_queue.rs
+++ capnp-futures/src/write_queue.rs
@@ -42,8 +42,8 @@ impl <M> Clone for Sender<M> where M: AsOutputSegments {
 }
 
 /// Creates a new WriteQueue that wraps the given writer.
-pub fn write_queue<W, M>(mut writer: W) -> (Sender<M>, impl Future<Output=Result<(),Error>> + 'static)
-    where W: AsyncWrite + Unpin + 'static, M: AsOutputSegments + 'static
+pub fn write_queue<W, M>(mut writer: W) -> (Sender<M>, impl Future<Output=Result<(),Error>> )
+    where W: AsyncWrite + Unpin , M: AsOutputSegments
 {
     let (tx, mut rx) = futures::channel::mpsc::unbounded();
 
@@ -69,10 +69,10 @@ pub fn write_queue<W, M>(mut writer: W) -> (Sender<M>, impl Future<Output=Result
     (sender, queue)
 }
 
-impl <M> Sender<M> where M: AsOutputSegments + 'static {
+impl <M> Sender<M> where M: AsOutputSegments  {
     /// Enqueues a message to be written. The returned future resolves once the write
     /// has completed.
-    pub fn send(&mut self, message: M) -> impl Future<Output=Result<M,Error>> + Unpin + 'static {
+    pub fn send(&mut self, message: M) -> impl Future<Output=Result<M,Error>> + Unpin  {
         let (complete, oneshot) = oneshot::channel();
 
         let _ = self.sender.unbounded_send(Item::Message(message, complete));
@@ -89,7 +89,7 @@ impl <M> Sender<M> where M: AsOutputSegments + 'static {
     /// Commands the queue to stop writing messages once it is empty. After this method has been called,
     /// any new calls to `send()` will return a future that immediately resolves to an error.
     /// If the passed-in `result` is an error, then the `WriteQueue` will resolve to that error.
-    pub fn terminate(&mut self, result: Result<(), Error>) -> impl Future<Output=Result<(),Error>> + Unpin + 'static {
+    pub fn terminate(&mut self, result: Result<(), Error>) -> impl Future<Output=Result<(),Error>> + Unpin  {
         let (complete, receiver) = oneshot::channel();
 
         let _ = self.sender.unbounded_send(Item::Done(result, complete));
