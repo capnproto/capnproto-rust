b8d54ec53fcf8bd04ba6408dc3d974308ddae417
Charlotte <driwer99@interia.pl>
2023-06-05 01:36:53 +0200

    wip, basically working with some issues
    Problem parts:
    - Clean up messy code, remove artifacts
    - Make proper tests
    - Currently, it doesn't support two capnp files with the same name - should we support that?
    - (Potentially not in scope of this issue) Build times seem awfully long in actions

---
+++ tests/usage_test.rs
@@ -0,0 +1,18 @@
+use capnp_import::capnp_import;
+
+//capnp_import!(["tests/example.capnp", "tests/folder-test/*.capnp"]);
+capnp_import!("tests/example.capnp");
+//capnp_import!("tests/example.capnp");
+//capnp_import!(["tests/folder-test/*"]);
+
+#[test]
+fn basic_file_test() {
+    //use example2_capnp::foo;
+    use example_capnp::date;
+    use example_capnp::person;
+}
+
+#[test]
+fn folder_test() {
+    //use person;
+}
