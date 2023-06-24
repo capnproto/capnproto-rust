6b4c262914331bf62a6785a854a4e2e75a293ae1
bootstrap-prime <bootstrap.prime@gmail.com>
2023-06-24 01:44:59 +0000

    feat: copy bytes of capnp into memory and dispense them on demand for developer convenience

--- tests/build_test.rs
+++ tests/build_test.rs
@@ -1,8 +1,6 @@
-use capnp_import;
-
-include!(concat!(env!("OUT_DIR"), "/binary_decision.rs"));
+include!(concat!(env!("OUT_DIR"), "/extract_bin.rs"));
 
 #[test]
 fn binary_decision_test() {
-    assert_eq!(CAPNP_BIN_PATH.is_empty(), false);
+    assert_eq!(commandhandle().unwrap().path().exists(), true);
 }
