af383af9f42c4310ba7d4698a154ad0da26d6e97
David Renshaw <dwrenshaw@gmail.com>
2023-10-26 09:37:45 -0400

    expand allocator testing

--- capnp/tests/scratch_space_heap_allocator.rs
+++ capnp/tests/scratch_space_heap_allocator.rs
@@ -1,11 +1,14 @@
+#![cfg(feature = "alloc")]
+
 use capnp::message;
 
 #[test]
-pub fn single_segment_allocator() {
-    let mut buffer = [capnp::word(0, 0, 0, 0, 0, 0, 0, 0); 200];
+pub fn scratch_space_heap_allocator() {
+    let mut buffer = capnp::Word::allocate_zeroed_vec(200);
     {
-        let allocator =
-            message::SingleSegmentAllocator::new(capnp::Word::words_to_bytes_mut(&mut buffer[..]));
+        let allocator = message::ScratchSpaceHeapAllocator::new(capnp::Word::words_to_bytes_mut(
+            &mut buffer[..],
+        ));
         let mut msg = message::Builder::new(allocator);
         msg.set_root("hello world!").unwrap();
 
