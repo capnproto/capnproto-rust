1688a45e7806865ca65a91517e4acb442c3681ef
quartox <jessewlord@gmail.com>
2024-01-04 17:19:36 +0000

    Use binary search to find field by name

--- capnp/src/schema.rs
+++ capnp/src/schema.rs
@@ -55,12 +55,24 @@ impl StructSchema {
         }
     }
 
-    /// Looks up a field by name. Returns `None` if no matching field is found.
+    /// Looks up a field by name using binary search. Returns `None` if no matching field is found.
     pub fn find_field_by_name(&self, name: &str) -> Result<Option<Field>> {
-        for field in self.get_fields()? {
-            if field.get_proto().get_name()? == name {
-                return Ok(Some(field));
+        self.raw.generic.members_by_name;
+        let mut lower: usize = 0;
+        let mut upper: usize = self.raw.generic.members_by_name.len();
+        let mut mid: usize = (lower + upper) / 2;
+        let (mut candidate_name, mut candidate_index) = self.raw.generic.members_by_name[mid];
+
+        while lower < upper {
+            if name == candidate_name {
+                return Ok(Some(self.get_fields()?.get(candidate_index)))
+            } else if candidate_name < name {
+                lower = mid + 1;
+            } else {
+                upper = mid;
             }
+            mid = (lower + upper) / 2;
+            (candidate_name, candidate_index) = self.raw.generic.members_by_name[mid];
         }
         Ok(None)
     }
