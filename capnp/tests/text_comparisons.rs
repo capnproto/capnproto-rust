3993ea256126c53f893890023a524cc7b316ca91
David Renshaw <dwrenshaw@gmail.com>
2024-01-11 12:01:16 -0500

    simplify text_comparisons test

--- capnp/tests/text_comparisons.rs
+++ capnp/tests/text_comparisons.rs
@@ -7,8 +7,8 @@ pub fn text_comparisons() {
     let mut msg1 = message::Builder::new_default();
     let mut msg2 = message::Builder::new_default();
 
-    msg1.set_root::<text::Reader>("abcde".into()).unwrap();
-    msg2.set_root::<text::Reader>("fghij".into()).unwrap();
+    msg1.set_root("abcde").unwrap();
+    msg2.set_root("fghij").unwrap();
 
     let str1 = msg1.get_root_as_reader::<text::Reader>().unwrap();
     let str2 = msg2.get_root_as_reader::<text::Reader>().unwrap();
