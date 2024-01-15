ddc5adc0d0bcafdd7ffae59a83af9bc920b0d33b
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-01-15 11:38:00 +0100

    Avoid external deps other than the capnp tool for regeneration of schemas
    On my Debian install /capnp/c++.capnp isn't resolved even after
    installing the capnproto package that includes the capnp tool used for
    parsing the capnp schemes.

--- regenerate-capnp-schema-code.sh
+++ regenerate-capnp-schema-code.sh
@@ -4,5 +4,5 @@ set -e
 set -x
 
 cargo build -p capnpc
-capnp compile -otarget/debug/capnpc-rust-bootstrap:capnp/src capnp/schema.capnp --src-prefix capnp/
+capnp compile -otarget/debug/capnpc-rust-bootstrap:capnp/src capnp/schema.capnp --src-prefix capnp/ -I. --no-standard-import
 rustfmt capnp/src/schema_capnp.rs
