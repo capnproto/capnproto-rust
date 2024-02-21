f099544e3731a16a899e7723efeda70ea6276670
David Renshaw <dwrenshaw@gmail.com>
2024-02-21 12:54:06 -0500

    re-add needed cfg attribute

--- capnp/src/private/capability.rs
+++ capnp/src/private/capability.rs
@@ -19,6 +19,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#![cfg(feature = "alloc")]
+
 use crate::any_pointer;
 use crate::capability::{Params, Promise, RemotePromise, Request, Results};
 use crate::MessageSize;
