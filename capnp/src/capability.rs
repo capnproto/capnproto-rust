e068cba1938c945aef151f8422cea03c5009e082
Apurva Mishra <59974350+mav3ri3k@users.noreply.github.com>
2024-07-10 13:21:14 +0530

    Fix little language mistake in capability.rs doc comment.

--- capnp/src/capability.rs
+++ capnp/src/capability.rs
@@ -19,7 +19,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-//! Hooks for for the RPC system.
+//! Hooks for the RPC system.
 //!
 //! Roughly corresponds to capability.h in the C++ implementation.
 
