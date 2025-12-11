143a0b1b7d4181aa545c8482865c2d4ad1bbb5ce
Ben Jackson <puremourning@gmail.com>
2025-12-11 22:59:55 +0000

    Remove redundant argument

--- capnp-json/test/build.rs
+++ capnp-json/test/build.rs
@@ -1,6 +1,5 @@
 fn main() {
     capnpc::CompilerCommand::new()
-        .crate_provides("external_crate", [0xe6f94f52f7be8fe2])
         .crate_provides("capnp_json", [0x8ef99297a43a5e34]) // json.capnp
         .file("test.capnp")
         .file("json-test.capnp")
